b71611c8806e2d80f09796cd6e9cbd9404a76349
ajlopez <webmaster@ajlopez.com>
2019-01-27 18:40:20 -0300

    Memoizing partitions

--- test/ps.js
+++ test/ps.js
@@ -20,3 +20,14 @@ exports['partitions for 2'] = function (test) {
     test.deepEqual(result[1], [ 1, 1 ]);
 };
 
+exports['partitions for 2 twice'] = function (test) {
+    var result = pfuns.ps(2);
+    var result2 = pfuns.ps(2);
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 2);
+    test.deepEqual(result[0], [ 2 ]);
+    test.deepEqual(result[1], [ 1, 1 ]);
+    test.strictEqual(result, result2);
+};
