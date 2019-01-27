cdf2c92bc6b1731e2a9b0e7c8be7dae759bf8269
ajlopez <webmaster@ajlopez.com>
2019-01-27 18:36:25 -0300

    Generate partitions

--- test/ps.js
+++ test/ps.js
@@ -10,3 +10,13 @@ exports['partitions for 1'] = function (test) {
     test.deepEqual(result[0], [ 1 ]);
 };
 
+exports['partitions for 2'] = function (test) {
+    var result = pfuns.ps(2);
+    
+    test.ok(result);
+    test.ok(Array.isArray(result));
+    test.equal(result.length, 2);
+    test.deepEqual(result[0], [ 2 ]);
+    test.deepEqual(result[1], [ 1, 1 ]);
+};
+
