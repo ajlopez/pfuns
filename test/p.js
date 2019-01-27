4ba787ed0c7698228ebc0c156c878a0b6f64ec17
ajlopez <webmaster@ajlopez.com>
2019-01-27 18:52:32 -0300

    More partition numbers in test

--- test/p.js
+++ test/p.js
@@ -7,4 +7,8 @@ exports['first partition numbers'] = function (test) {
     test.equal(pfuns.p(3), 3);
     test.equal(pfuns.p(4), 5);
     test.equal(pfuns.p(5), 7);
+    test.equal(pfuns.p(6), 11);
+    test.equal(pfuns.p(7), 15);
+    test.equal(pfuns.p(8), 22);
+    test.equal(pfuns.p(9), 30);
 };
