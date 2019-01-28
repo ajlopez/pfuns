582a13bfcbce387bee9492b616640d2ef88fa4be
ajlopez <webmaster@ajlopez.com>
2019-01-28 09:16:04 -0300

    Additional numbers in test

--- test/p.js
+++ test/p.js
@@ -12,3 +12,12 @@ exports['first partition numbers'] = function (test) {
     test.equal(pfuns.p(8), 22);
     test.equal(pfuns.p(9), 30);
 };
+
+// from https://oeis.org/A000041
+
+exports['next partition numbers'] = function (test) {
+    const expected = [  42, 56, 77, 101, 135, 176, 231, 297, 385, 490, 627, 792, 1002, 1255, 1575, 1958 ];
+
+    for (var k = 0; k < expected.length; k++)
+        test.equal(pfuns.p(k + 10), expected[k]);
+};
